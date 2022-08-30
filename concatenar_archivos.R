f176e7944571d1dc8043200e4e54c7725d9bd6b1
andrespan <andresabstract@gmail.com>
2022-08-30 12:53:37 -0700

    añadi substrings

--- concatenar_archivos.R
+++ concatenar_archivos.R
@@ -1,23 +1,33 @@
 ##buscar con grep un identificador 
-identificador1 <- ">5mSIPHEX1_0-scaffold_1104_c1_1"
-identificador2 <- ">5mSIPHEX1_0-scaffold_1104_c1_2"
+identificador1 <- ">*-scaffold_*_*_*"
+identificador1
+#identificador1 <-">*"
+#identificador2 <- ">5mSIPHEX1_0-scaffold_1104_c1_2"
 path <- readLines('Datos/5mSIPHEX1_0_short.faa')
 #path <- read.delim('Datos'/'5mSIPHEX1_0_short.faa',header=TRUE,sep = " ")
 #path <- read.table(file('Datos'/'5mSIPHEX1_0_short.faa'),row.names=0,sep='\t')
-grep(identificador1,path) 
-grep(identificador2,path)
+#name_numeric  <- grep(identificador1,path) 
+#as.numeric(as.factor(identificador1))
+#grep(identificador2,path)
 #grep(">5mSIPHEX1_0-scaffold_1104_c1_1", path)
 #grep(">5mSIPHEX1_0-scaffold_1104_c1_2", path)
 #imprime las lineas desde el primero hasta el segundo ID
-path[grep(identificador1,path):grep(identificador2,path)]
+#path[grep(identificador1,path):grep(identificador1,path)]
 #imprime hasta la segunda linea del archivo
-path[grep(identificador1,path):2]
+name <-path[grep(identificador1,path)]
+name
+substring (name, 1, 32)
+substring (name, 32, nchar)  
+name
+#name_numeric  <- as.numeric(as.factor(name))
+#sequence <- for (name in path) {nchar(identificador1)+1}
+sequence
 
-path[grep(identificador2,path):4]
+path[grep(identificador1,path)]
+
 
 
 #name 
 #path
 identificador1
-setwd()
-getwd()
+#setwd() y getwd()
