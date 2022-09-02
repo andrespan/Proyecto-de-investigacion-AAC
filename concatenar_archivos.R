1419860931e0cb7b418d0e6f67a08a98f39f73dc
andrespan <andresabstract@gmail.com>
2022-09-02 16:57:46 -0500

    agregue funcion a concatenar archivos y cree el archivo identificador

--- concatenar_archivos.R
+++ concatenar_archivos.R
@@ -4,6 +4,28 @@ identificador2 <- ">5mSIPHEX1_0-scaffold_1104_c1_2"
 #cargar el archivo de secuencias
 Secuencias_file <- readLines('Datos/5mSIPHEX1_0_short.faa')
 Secuencias_file
+#file <- readLines('Datos/*.faa')
+#Obtener ordenadamente ID, coordendas 1 y 2 y aminoacidos
+
+archivo_txt <- function(file){ 
+  identificador2 <- ">5mSIPHEX1_0-scaffold_1104_c1_2"
+  grep_line <- grep(value = TRUE, identificador2,file)
+  #cordenada1
+  element <-strsplit(grep_line, "#")
+  element[[1]][2]
+  coordenada2 <- as.integer( element[[1]][2])
+  #cordenada 2
+  element[[1]][3]
+  coordenada1 <- as.integer( element[[1]][3])
+  df <- data.frame(matrix(ncol = 2, nrow = 0))
+  colnames(df) <-c("coordenada1", "coordenada2")
+  #rellenar las filas de el df
+  df[1,] <-c(coordenada1,coordenada2)
+  return (df)
+}
+
+archivo_txt(Secuencias_file)
+
 #Encontrar al identificador2 
 #Ejemplo
 #">5mSIPHEX1_0-scaffold_1104_c1_2 # 235 # 1749 # 1 # ID=11_2;partial=00;start_type=ATG;rbs_motif=AGGAG;rbs_spacer=5-10bp;gc_cont=0.637"
@@ -12,9 +34,7 @@ grep_line <- grep(value = TRUE, identificador2,Secuencias_file)
 grep_index <-grep(identificador2,Secuencias_file)
 #Obtener coordenadas y ID
 #cordenada 2
-element <-strsplit(grep_line, "#")
-element[[1]][3]
-coordenada2 <- as.integer( element[[1]][3])
+
 
 #cordenada 1
 element[[1]][2]
@@ -25,12 +45,6 @@ coordenada1 <- as.integer( element[[1]][2])
 #obtener secuencia de aminoacidos
 aminoacid_sec <- Secuencias_file[grep_index+1]
 aminoacid_sec
-#Obtener ordenadamente ID, coordendas 1 y 2 y aminoacidos
-
-archivo_txt <- function(Secuencias_file)
-{return (cat(identificador2, coordenada1, coordenada2, aminoacid_sec))
-}
-archivo_txt(Secuencias_file)
 
 #obtieniendo un archivo txt con el ID, las cordenadas ,la anotacion y la secuencia
 #Y un archivo fasta con identificadores de RAST
@@ -39,7 +53,10 @@ archivo_txt(Secuencias_file)
 #5m es la profundidad
 #SI Enriquecimiento
 #PHEX es el  la molecula que se uso
-#
+#1_0 es el ID 
+#-scaffold
+#1104 es el identificador del scaffold
+#c1_2  es el ID del gen
 
 #haciendo un for para todos los IDs
 for (">" in Secuencias_file) {
