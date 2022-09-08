76c3c658cfc1607f08775572b4f23dd175e3a72f
andrespan <andresabstract@gmail.com>
2022-09-08 15:31:35 -0500

    Avance de la funcion(aun no corre)

--- Evomining/Function_Idslist.R
+++ Evomining/Function_Idslist.R
@@ -1,1 +1,17 @@
 #un archivo de entrada  y una lista de salida
+#cargamos el archivo
+Secuencias_file <- readLines('Datos/5mSIPHEX1_0_short.faa')
+#creamos una funcion
+Idlist<- function(file){
+  #identificamos todas las lineas que contienen el ID
+  grep_line <- grep(value = TRUE, ">",file)
+  #recortamos por el espacio
+  Id <-strsplit(grep_line, " #")
+  #el valor numerico de cada linea 
+  grep_index <-grep(">",file)
+  ID<-Id[[grep_index]][1]
+  
+  return (ID)
+}
+Idlist(Secuencias_file)
+
