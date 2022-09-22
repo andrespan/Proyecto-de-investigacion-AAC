2894ff294ab1184e2cbc9d59fff8a9f7e8b0cd2b
andrespan <andresabstract@gmail.com>
2022-09-22 11:28:09 -0500

    ko

--- Evomining/read_koutput.R
+++ Evomining/read_koutput.R
@@ -1,8 +1,12 @@
 #con la funcion read_ko buscar en la tabla la columna 3 (el numero de KO)
+library(devtools)
+install_github("mirnavazquez/RbiMs")
+library(rbims)
 library("rbims")
+
 k0<-read_ko("Datos/")
 k0
-class(k0)
+getclass(k0)
 #df1 <- k0[k0$KO == 1, ]
 #df1
 
@@ -10,7 +14,9 @@ class(k0)
 column3 <- k0[,3]
 column3
 #funcion atomo 
-atom <-
+atom <-function(df){
+  grep_index <-grep("*",df)
+}
 
 # doy un ID y regresar una function metabolica 
 #aplicarla a todos los Id
