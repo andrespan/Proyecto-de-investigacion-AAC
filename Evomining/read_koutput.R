ed701adfa2f58e3b99bf9d6afc18f58b6d7e3b0b
andrespan <andresabstract@gmail.com>
2022-09-26 14:55:48 -0500

    read ko actualizacion

--- Evomining/read_koutput.R
+++ Evomining/read_koutput.R
@@ -3,54 +3,75 @@
 #install_github("mirnavazquez/RbiMs")
  #library(rbims)
 library("rbims")
-
+#dataframe
 k0<-read_ko("Datos/5mSIPHEX1_0_short.txt")
 k0
-getclass(k0)
+class(k0)
 #df1 <- k0[k0$KO == 1, ]
 #df1
-
-anotacion <- function(file){
+#anotacion <- function(file){
   #identificamos todas las lineas que contienen ">" 
-  grep_index <-grep("*",file)
+  #grep_index <-grep("*",file)
   #le pedimos esas lineas al archivo
-  ID_lines <- file[grep_index]
+  #ID_lines <- file[grep_index]
   #recortamos por el  " #"
-  Id <-strsplit(ID_lines, )
+  #Id <-strsplit(ID_lines, )
   #de cada seleccion elegir la linea [1]
   #ID <-sapply(Id, `[`, 3, simplify = FALSE)
   #el valor numerico de cada linea 
   #grep_index <-grep(">",file)
-  return (Id)
-}
-#tr<-
-anotacion(k0)
+  #return (Id)
+#}
 
-class(str)
+#anotacion(k0)
+
+#class(str)
 #Seleccionar la columna tres 
-column1 <- k0[,1]
-column1
-ID<-column1[1]
-ID
-class(ID) 
+#column1 <- k0[,1]
+#column1
+#ID<-column1[1]
+#ID
+#class(ID) 
+#variable id de prueba
+id_prueba <- "5mSIPHEX1_0_scaffold_1104_c1_2"
+id_prueba
+
+#prueba grep
+#grep("5mSIPHEX1_0_scaffold_1104_c1_2",k0$Scaffold_name)
+#k0$Scaffold_name
+#subset(k0, grepl("5mSIPHEX1_0_scaffold_1104_c1_2", k0$Scaffold_name))
+#colnames(k0)
+#My.Data <- k0
+#grep("5mSIPHEX1_0_scaffold_1104_c1_2", My.Data$Scaffold_name)
+grep_id <-k0[grep("5mSIPHEX1_0_scaffold_1104_c1_2", k0$Scaffold_name), ]
+grep_id
+
+#subset(My.Data, startsWith(as.character(Scaffold_name), "5mSIPHEX1_0_scaffold_1104_c1_2"))
+#library(dplyr)
+#library(stringr)
+#My.Data %>% filter(str_detect(Scaffold_name, '5mSIPHEX1_0_scaffold_1104_c1_2'))
+
 #funcion atomo 
-atom <-function(df){
-  column1 <- df[1,]
+# doy un ID y regresar una function metabolica
+#5mSIPHEX1_0-scaffold_1104_c1_2 K02056
+ID_to_metabolic <-function(id,df){
+  #grep_id <- grep(value = TRUE,id,df)
+  grep_id <-df[grep(id, df$Scaffold_name), ]
+  #grep_id
   #column1 <-strsplit(ID_lines,\)[1]
-  grep_id <- grep(value = TRUE,id,column1)
-  funcion<-column1[3]
+  metabolic<-grep_id[3]
   #grep_id <- grep(value = TRUE,2,df)
   #column3 <- df[,3]
   #grep_index <-grep(value = TRUE,id,df)
   #ID_ko <-grep_index[1]
-  return(funcion)
+  return(metabolic)
 }
 
-atom(k0)
+ID_to_metabolic(id_prueba,k0)
 #aplicamos funcion para todos los ids
 library(plyr)
-ddply(.data =column1,
-      .fun= function(x) atom(x))
+ldply(.data =column1,
+      .fun= function(x) ID_to_metabolic(x))
 
 
 
@@ -58,7 +79,7 @@ ddply(.data =column1,
 k0
 
 class(funcion)
-# doy un ID y regresar una function metabolica 
+
 #aplicarla a todos los Id
 #con la funcion dply 
 #pegarla al dataframe
