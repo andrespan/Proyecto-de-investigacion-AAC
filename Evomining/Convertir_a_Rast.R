fac193201489aed742a518625495eb711e56e9b1
andrespan <andresabstract@gmail.com>
2022-09-29 15:34:01 -0500

    ko

--- Evomining/Convertir_a_Rast.R
+++ Evomining/Convertir_a_Rast.R
@@ -69,7 +69,44 @@ library(plyr)
 ldply(.data = listof_ids,
       .fun= function(x) archivo_txt(Secuencias_file,x))
 
-
-
 #-------------------------------------------------------------------------------
 #Agregamos la columna function al df entre la columna 3 y 4
+#incluimos la funcion ID_to_metabolic
+
+library("rbims")
+#dataframe K0 es e output de la funcion read_ko de rbims
+k0<-read_ko("Datos/5mSIPHEX1_0_short.txt")
+k0
+#variable id de prueba
+id_prueba <- "5mSIPHEX1_0_scaffold_1104_c1_2"
+
+#prueba grep
+#grep("5mSIPHEX1_0_scaffold_1104_c1_2", My.Data$Scaffold_name)
+grep_id <-k0[grep("5mSIPHEX1_0_scaffold_1104_c1_2", k0$Scaffold_name), ]
+grep_id
+
+#subset(My.Data, startsWith(as.character(Scaffold_name), "5mSIPHEX1_0_scaffold_1104_c1_2"))
+#library(dplyr)
+#library(stringr)
+#My.Data %>% filter(str_detect(Scaffold_name, '5mSIPHEX1_0_scaffold_1104_c1_2'))
+
+#funcion atomo 
+# doy un ID y regresar una function metabolica
+#5mSIPHEX1_0-scaffold_1104_c1_2 K02056
+ID_to_metabolic <-function(id,df){
+  grep_id <-df[grep(id, df$Scaffold_name), ]
+  metabolic<-grep_id[3]
+  return(metabolic)
+}
+
+ID_to_metabolic(id_prueba,k0)
+
+#ahora lo voy a aplicar a una lista de IDs 
+Lista_IDs <- k0[,1]
+column1
+#aplicamos funcion para todos los ids
+library(plyr)
+ldply(.data =column1,
+      .fun= function(x) ID_to_metabolic(x,k0))
+
+
--- Evomining/read_koutput.R
+++ Evomining/read_koutput.R
@@ -1,27 +1,17 @@
 #con la funcion read_ko buscar en la tabla la columna 3 (el numero de KO)
 #library(devtools)
 #install_github("mirnavazquez/RbiMs")
- #library(rbims)
 library("rbims")
-#dataframe
+#dataframe proviene del output de la funcion read_ko 
 k0<-read_ko("Datos/5mSIPHEX1_0_short.txt")
 k0
-class(k0)
+#class(k0)
 
-
-
-#anotacion(k0)
-
-#class(str)
-
-#ID
-#class(ID) 
 #variable id de prueba
 id_prueba <- "5mSIPHEX1_0_scaffold_1104_c1_2"
 id_prueba
 
 #prueba grep
-
 #grep("5mSIPHEX1_0_scaffold_1104_c1_2", My.Data$Scaffold_name)
 grep_id <-k0[grep("5mSIPHEX1_0_scaffold_1104_c1_2", k0$Scaffold_name), ]
 grep_id
@@ -43,11 +33,11 @@ ID_to_metabolic <-function(id,df){
 ID_to_metabolic(id_prueba,k0)
 
 #ahora lo voy a aplicar a una lista de IDs 
-Lista_IDs <- k0[,1]
-column1
+Lista_IDs <- k0$Scaffold_name
+Lista_IDs
 #aplicamos funcion para todos los ids
 library(plyr)
-ldply(.data =column1,
+ldply(.data =Lista_IDs,
       .fun= function(x) ID_to_metabolic(x,k0))
 
 #
