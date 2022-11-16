78d6bc2e578226044ceb48c027cae88f20740d18
andrespan <andresabstract@gmail.com>
2022-11-16 16:49:51 -0600

    feature_id y datos

--- Evomining/feature_id.R
+++ Evomining/feature_id.R
@@ -1,1 +1,8 @@
 #crear columna feature id
+
+# los feature_id van a ir 100000 20000000....420000
+library("readr")
+gtdbK_file <- read_table('Datos/gtdbtk.bac120.summary.tsv')
+gtdbK_file$classification
+gtdbK_file$user_genome
+
